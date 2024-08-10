c55b6868b34d94879c78949b93bd0548658a189c
AxLamelas <alamelas@ua.pt>
2024-08-10 10:54:19 +0100

    Added ArrayParameter

--- src/ModelFlatten.jl
+++ src/ModelFlatten.jl
@@ -7,9 +7,18 @@ using Reexport
 @reexport using Bijectors
 @reexport using Distributions
 
-using Bijectors: Bijector
+using Bijectors: Bijector, TruncatedBijector, Stacked, inverse
+
+export UnboundedBijector, vector_and_transformation, TruncatedBijector, setup_transforms
 
 
+const UnboundedBijector{T} = TruncatedBijector{T,T}
+
+function UnboundedBijector(::Type{T}) where {T <: Number}
+    return Bijectors.TruncatedBijector(typemin(T),typemax(T))
+end
+
+const ArrayParameter{T,B} = Tuple{A,B} where {T <: Number, A <: AbstractArray{T}, B <: Bijector}
 const Parameter{T,B} = Tuple{T,B} where {T <: Number, B <: Bijector}
 Base.length(x::Parameter) = length(x[1])
 Base.show(io::IO, p::Parameter) = print(io,"P[$(p[1])]")
@@ -18,7 +27,7 @@ value(x::Parameter) = x[1]
 
 # Base.length(x::Dirichlet) = length(x.alpha)
 
-const ParameterLike = Union{Parameter{<:Number},Distribution,Bijector}
+const ParameterLike = Union{ArrayParameter, Parameter,Distribution,Bijector}
 
 @inline wrap(x) = if x isa ParameterLike
     (x,)
@@ -28,6 +37,16 @@ end
 @inline tuplejoin(x) = x
 @inline tuplejoin(x, y) = (wrap(x)...,wrap(y)...)
 @inline tuplejoin(x, y, z...) = (wrap(x)..., tuplejoin(y, z...)...)
+
+function _length(x)
+    return if !applicable(iterate,x) 
+        length(x)
+    elseif isempty(x)
+        0
+    else
+        sum(length(xi) for xi in x)
+    end
+end
 
 function flatten(x)
     unflatten_Fixed(_) = x
@@ -39,18 +58,22 @@ function flatten(x::ParameterLike)
     return x, unflatten
 end
 
+function flatten(x::Distribution)
+    # unflatten only works of samples from the distribution
+    l = length(x)
+    if l == 1
+        unflatten_single(v) = only(v)
+        return x, unflatten_single
+    end
+
+    unflatten(v) = v[1:l]
+    return x, unflatten
+end
+
 function flatten(x::AbstractArray) 
     x_vecs_and_backs = map(val -> flatten(val), x)
     x_vecs, x_backs = first.(x_vecs_and_backs), last.(x_vecs_and_backs)
-    lengths = reshape(map(x_vecs) do x
-        if !applicable(iterate,x) 
-            length(x)
-        elseif isempty(x)
-            0
-        else
-            sum(length(xi) for xi in x)
-        end
-    end,:)
+    lengths = reshape(map(_length,x_vecs),:)
     sz = cumsum(lengths)
     function unflatten_to_vec(v)
         reshape(map(x_backs, lengths, sz) do x_back, l, s
@@ -64,15 +87,7 @@ end
 function flatten(x::AbstractVector) 
     x_vecs_and_backs = map(val -> flatten(val), x)
     x_vecs, x_backs = first.(x_vecs_and_backs), last.(x_vecs_and_backs)
-    lengths = map(x_vecs) do x
-        if !applicable(iterate,x) 
-            length(x)
-        elseif isempty(x)
-            0
-        else
-            sum(length(xi) for xi in x)
-        end
-    end
+    lengths = map(_length,x_vecs)
     sz = cumsum(lengths)
     function unflatten_to_vec(v)
         map(x_backs, lengths, sz) do x_back, l, s
@@ -85,15 +100,7 @@ end
 function flatten(x::Tuple) 
     x_vecs_and_backs = map(val -> flatten(val), x)
     x_vecs, x_backs = first.(x_vecs_and_backs), last.(x_vecs_and_backs)
-    lengths = map(x_vecs) do x
-        if !applicable(iterate,x) 
-            length(x)
-        elseif isempty(x)
-            0
-        else
-            sum(length(xi) for xi in x)
-        end
-    end
+    lengths = map(_length,x_vecs)
     sz = cumsum(lengths)
     function unflatten_to_Tuple(v)
         map(x_backs, lengths, sz) do x_back, l, s
@@ -111,6 +118,39 @@ function flatten(x::NamedTuple{N}) where {N}
     end
     return x_vec, unflatten_to_NamedTuple
 end
+
+function vector_and_transformation(θ0::NTuple{N,T}) where {N,T <: Union{Parameter,ArrayParameter}}
+    bijectors = bijector.(θ0)
+
+    to_unconstrained = Stacked(bijectors)
+    to_constrained = inverse(to_unconstrained)
+
+    return (;θ0 = to_unconstrained(vcat(value.(θ0)...)), to_unconstrained, to_constrained)
+end
+
+function vector(θ0::NTuple{N,T}) where {N,T <: Union{Parameter,ArrayParameter}}
+    bijectors = bijector.(θ0)
+    to_unconstrained = Stacked(bijectors)
+
+    return to_unconstrained(vcat(value.(θ0)...))
+end
+
+function setup_transforms(priors::NamedTuple)
+    bs = bijector.(values(priors))
+    ranges = UnitRange{Int64}[]
+    idx = 1
+    for p in priors
+        len = length(p)
+        push!(ranges, idx:idx+len-1)
+        idx += len
+    end
+
+    sb = Stacked(bs, ranges)
+    isb = inverse(sb)
+
+    return sb, isb
+end
+
 
 
 end # module ModelFlatten
